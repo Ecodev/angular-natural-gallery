b3025c2b0204531f759f0786e0a617b7698b6db2
Samuel Baptista <samuel.baptista@ecodev.ch>
2017-11-29 12:12:02 +0100

    First commit
    Deploy https://github.com/jvandemo/generator-angular2-library based module

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SampleComponent } from './sample.component';
+import { SampleDirective } from './sample.directive';
+import { SamplePipe } from './sample.pipe';
+import { SampleService } from './sample.service';
+
+export * from './sample.component';
+export * from './sample.directive';
+export * from './sample.pipe';
+export * from './sample.service';
+
+console.log('asdf');
+
+@NgModule({
+    imports: [
+        CommonModule,
+    ],
+    declarations: [
+        SampleComponent,
+        SampleDirective,
+        SamplePipe,
+    ],
+    exports: [
+        SampleComponent,
+        SampleDirective,
+        SamplePipe,
+    ],
+})
+export class SampleModule {
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SampleModule,
+            providers: [SampleService],
+        };
+    }
+}
