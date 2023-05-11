840b7970fe1131bc0976334fbda8d7a1a1da07fe
Sam Baptista <samuel.baptista@ecodev.ch>
2023-05-11 17:31:11 +0200

    typo

--- projects/angular-natural-gallery/src/lib/natural-gallery.module.ts
+++ projects/angular-natural-gallery/src/lib/natural-gallery.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NaturalGalleryComponent} from './natural-gallery/natural-gallery.component';
 
+
 @NgModule({
     declarations: [NaturalGalleryComponent],
     imports: [CommonModule],
