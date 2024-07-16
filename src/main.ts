86ebf3a7bcb3e5046f81445c2e7542c2dc2dc202
Adrien Crivelli <adrien.crivelli@gmail.com>
2024-07-16 21:54:15 +0900

    Leverage `eventCoalescing` #10322
    For small performance boost when handling events

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import {enableProdMode, importProvidersFrom} from '@angular/core';
+import {enableProdMode, provideZoneChangeDetection} from '@angular/core';
 import {environment} from './environments/environment';
 import {AppComponent} from './app/app.component';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
+import {bootstrapApplication} from '@angular/platform-browser';
 
 if (environment.production) {
     enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule), provideAnimationsAsync()],
+    providers: [provideZoneChangeDetection({eventCoalescing: true}), provideAnimationsAsync()],
 }).catch((err: unknown) => {
     console.error(err);
 });
