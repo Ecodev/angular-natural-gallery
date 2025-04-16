aab26c702321bc2ec62879510a7a4d3b7ecbeb7d
Adrien Crivelli <adrien.crivelli@gmail.com>
2025-04-16 13:35:27 +0900

    Enable `prefer-contextual-for-variables` #10723

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -155,7 +155,6 @@ export default tseslint.config(
         files: ['**/*.html'],
         extends: [...angular.configs.templateAll],
         rules: {
-            '@angular-eslint/template/prefer-contextual-for-variables': 'off', // TODO Angular 19: enable and autofix
             '@angular-eslint/template/alt-text': 'off', // We don't care as much as we should about a11y
             '@angular-eslint/template/button-has-type': 'off',
             '@angular-eslint/template/click-events-have-key-events': 'off', // We don't care as much as we should about a11y
