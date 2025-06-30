f973dece22e85f231f7071ff53bea41c8189ef54
Adrien Crivelli <adrien.crivelli@gmail.com>
2025-06-30 12:42:59 +0900

    Drop unused @angular/animations #11561

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import {enableProdMode, provideZoneChangeDetection} from '@angular/core';
 import {environment} from './environments/environment';
 import {AppComponent} from './app/app.component';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {bootstrapApplication} from '@angular/platform-browser';
 
 if (environment.production) {
@@ -9,7 +8,7 @@ if (environment.production) {
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [provideZoneChangeDetection({eventCoalescing: true}), provideAnimationsAsync()],
+    providers: [provideZoneChangeDetection({eventCoalescing: true})],
 }).catch((err: unknown) => {
     console.error(err);
 });
