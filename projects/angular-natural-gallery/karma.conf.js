85a9bf332b7e0e785fdd51a7c8b6dbb82e9bfaa0
Adrien Crivelli <adrien.crivelli@gmail.com>
2025-09-10 19:13:19 +0900

    Angular 20.2 and all JS deps #11431
    Except:
    
    - @apollo/client, apollo-angular is not ready
    - @types/node, we run on node 22

--- projects/angular-natural-gallery/karma.conf.js
+++ projects/angular-natural-gallery/karma.conf.js
@@ -8,13 +8,12 @@ process.env.CHROME_BIN = require('@playwright/test').chromium.executablePath();
 module.exports = function (config) {
     config.set({
         basePath: '',
-        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        frameworks: ['jasmine'],
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
-            require('@angular-devkit/build-angular/plugins/karma'),
         ],
         client: {
             clearContext: false, // leave Jasmine Spec Runner output visible in browser
--- src/test.ts
+++ src/test.ts
@@ -2,13 +2,7 @@
 
 import 'zone.js/testing';
 import {getTestBed} from '@angular/core/testing';
-import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
-
-declare const require: any;
+import {BrowserTestingModule, platformBrowserTesting} from '@angular/platform-browser/testing';
 
 // First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
+getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowserTesting());
